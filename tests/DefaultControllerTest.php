5902d6dd564f63b8b37d216557ed67a24dc69c5f
luisibal <luizenrikien@gmail.com>
2019-12-09 22:45:11 -0700

    testing isolation

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -2,18 +2,50 @@
 
 namespace App\Tests;
 
+use App\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/home');
-        // var_dump($client->getResponse()->getContent());
+    private $entityManager;
 
-        // var_dump($crawler->filter('h1')->text());
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Hello', $crawler->filter('h1')->text());
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+
+        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+
+        $this->entityManager->beginTransaction();
+        $this->entityManager->getConnection()->setAutoCommit(false);
+    }
+
+    protected function tearDown()
+    {
+        $this->entityManager->rollback();
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testSomething($url)
+    {
+        $crawler = $this->client->request('GET', $url);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $video = $this->entityManager->getRepository(Video::class)->find(3);
+        $this->entityManager->remove($video);
+        $this->entityManager->flush();
+    }
+
+    public function provideUrls()
+    {
+        return [
+            ['/home'],
+            ['login'],
+        ];
     }
 }
